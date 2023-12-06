b88786f1243613cc206fd3aa059f77d662e7e8e5
Jordi <jordifransen@hotmail.com>
2023-12-06 19:00:12 +0100

    Map voor output en rerouting van outfile files

--- Run.sh
+++ Run.sh
@@ -7,4 +7,4 @@
 # Opstart Script
 
 /home/franco/Desktop/wifi-scanner/newvenv/bin/python3 /home/franco/Desktop/wifi-scanner/main.py
-sudo cp /scan.json /home/franco/Desktop/wifi-scanner/scan.json
+sudo cp /scan.json /home/franco/Desktop/wifi-scanner/Output/scan.json
