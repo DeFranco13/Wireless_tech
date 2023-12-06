d585d6f9304ebb1ad314c922e8a84ad8816b682d
RaspPi <jordi.fransen@student.ap.be>
2023-12-06 17:41:43 +0100

    Auto pull on reboot

--- Run.sh
+++ Run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Pull van project
+
+./home/franco/Desktop/wifi-scanner/GithubPull.sh
+
+# Opstart Script
+
 /home/franco/Desktop/wifi-scanner/newvenv/bin/python3 /home/franco/Desktop/wifi-scanner/main.py
 sudo cp /scan.json /home/franco/Desktop/wifi-scanner/scan.json
