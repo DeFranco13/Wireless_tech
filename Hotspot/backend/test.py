b7a5f7a199658f13684af232ca76872091e943ad
joeydewaal <dewaaljoey@gmail.com>
2023-12-12 13:42:39 +0100

    OPEN, WEP support + sort() fix kut javascirpt

--- Hotspot/backend/test.py
+++ Hotspot/backend/test.py
@@ -1,7 +1,7 @@
 import requests as r
 import time
 from random import randint
-wpa = ["WPA1", "WPA2", "WPA3"]
+wpa = ["WPA1", "WPA2", "WPA3", "OPEN", "WEP"]
 while True:
     data = {
             "SSID": "Fransen",
@@ -9,7 +9,7 @@ while True:
             "CHAN": randint(0,30), #16
             "RATE": randint(50, 200),
             "SIGNAL": randint(50,200),
-            "SECURITY": wpa[randint(0,2)] # "WPA1"
+            "SECURITY": wpa[randint(0,len(wpa)-1)] # "WPA1"
         }
 
     res = r.post("http://localhost:8080/", json=data)
